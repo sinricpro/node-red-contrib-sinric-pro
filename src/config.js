0cabbacfe811376f69b8447925716d2bf6c386e5
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2023-01-29 11:20:54 +0700

    changed to wss

--- src/config.js
+++ src/config.js
@@ -1,11 +1,4 @@
-/*
- *  Copyright (c) 2019-2023 Sinric. All rights reserved.
- *  Licensed under Creative Commons Attribution-Share Alike (CC BY-SA)
- *
- *  This file is part of the Sinric Pro (https://github.com/sinricpro/)
- */
-
-const API_ENDPOINT = "ws://ws.sinric.pro";
+const API_ENDPOINT = 'wss://ws.sinric.pro';
 
 module.exports = {
     API_ENDPOINT
